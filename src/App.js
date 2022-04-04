efb846489ba3296f1ba1d88a82352b799fa33d16
Daniel Castillo <dcastillo@myemail.com>
2022-04-03 20:56:06 -0500

    complete project - first deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [advice, setAdvice] = useState("");
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  const fetchAdvice = async () => {
+    try {
+      const resp = await axios.get("https://api.adviceslip.com/advice");
+      setAdvice(resp.data.slip.advice);
+    } catch (error) {
+      console.log("oopsie... there is an error getting your advice");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="card">
+        <h1 className="heading">{advice}</h1>
+        <button className="button" onClick={fetchAdvice}>
+          <span>Give me more advice</span>
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
